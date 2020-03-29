6a123bae919a9c483e7beb5b1a78fec6a6e275de
Jonathancsr <jonathancsrr@gmail.com>
2020-03-29 17:07:59 -0300

    login page finish

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './global.css';
+
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
